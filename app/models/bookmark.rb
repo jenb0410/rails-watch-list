169226a9e5faed20104d05db1dd2c22518431c89
Jennifer Braun <jenb04100@gmail.com>
2023-02-23 13:56:10 +0100

    lalala

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
-  belongs_to :list_id
-  belongs_to :movie_id
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: { minimum: 7 }
+  validates :movie_id, presence: true
+  validates :list_id, presence: true
+  validates_uniqueness_of :list_id, :scope => [:movie_id]
 end
